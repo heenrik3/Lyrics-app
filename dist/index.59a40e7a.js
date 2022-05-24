15009daa7c00c9a9882f25220fb161905b265b08
heenrik3 <henriqlh@gmail.com>
2022-05-23 21:32:57 -0300

    3

--- dist/index.59a40e7a.js
+++ dist/index.59a40e7a.js
@@ -527,16 +527,64 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"8lRBv":[function(require,module,exports) {
 var _ripple = require("@material/ripple");
-// buttonElement.setAttribute('style', 'disabled')
 var _textfield = require("@material/textfield");
-const buttonElement = document.querySelector('.mdc-button');
-const buttonRipple = new _ripple.MDCRipple(buttonElement);
-const textFields = document.querySelectorAll('.mdc-text-field');
-textFields.forEach((textField)=>{
-    new _textfield.MDCTextField(textField);
-}) // const textField = new MDCTextField()
- // buttonElement.disabled = false
-;
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        }
+    } catch (e) {
+        console.log('Error fetching: ' + url);
+    }
+}
+async function getAllData(artist, song) {
+    const lyric_api = 'https://api.lyrics.ovh/v1/';
+    const image_api = 'https://imsea.herokuapp.com/api/1?q=';
+    let lyrics = undefined;
+    let picture = undefined;
+    const lyric_url = lyric_api + artist + '/' + song;
+    const image_url = image_api + artist;
+    await fetchData(lyric_url).then((data)=>{
+        if (data) lyrics = data.lyrics;
+    });
+    await fetchData(image_url).then((data)=>{
+        if (data) picture = data.results[0];
+    });
+    return {
+        lyric: lyrics,
+        picture: picture
+    };
+// console.log(lyrics, picture)
+}
+function renderData(artist, song, data) {
+    const artist_picture = document.querySelector('.search__image');
+    const artist_name_text = document.querySelector('.search__artist');
+    const song_name_text = document.querySelector('.search__song');
+    const lyrics_text = document.querySelector('.search__lyrics');
+    artist_picture.src = data.picture;
+    artist_name_text.textContent = artist.toUpperCase();
+    song_name_text.textContent = song.toUpperCase();
+    lyrics_text.textContent = data.lyric;
+}
+function start() {
+    const textFields = document.querySelectorAll('.mdc-text-field');
+    const button = document.querySelector('.mdc-button');
+    const buttonRipple = new _ripple.MDCRipple(button);
+    const artist_text_field = new _textfield.MDCTextField(textFields[0]);
+    const song_text_field = new _textfield.MDCTextField(textFields[1]);
+    button.addEventListener('click', ()=>{
+        const artist = artist_text_field.value;
+        const song = song_text_field.value;
+        if (!artist || !song) return;
+        getAllData(artist, song).then((data)=>{
+            console.log(data);
+            renderData(artist, song, data);
+        });
+    });
+}
+start();
 
 },{"@material/ripple":"jRAE3","@material/textfield":"iAIQW"}],"jRAE3":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,77 @@
 import { MDCRipple } from '@material/ripple'
-
-const buttonElement = document.querySelector('.mdc-button')
-
-const buttonRipple = new MDCRipple(buttonElement)
-
-// buttonElement.setAttribute('style', 'disabled')
-
 import { MDCTextField } from '@material/textfield'
 
-const textFields = document.querySelectorAll('.mdc-text-field')
+async function fetchData(url) {
+  try {
+    const response = await fetch(url)
 
-textFields.forEach((textField) => {
-  new MDCTextField(textField)
-})
-// const textField = new MDCTextField()
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    } else false
+  } catch (e) {
+    console.log('Error fetching: ' + url)
+  }
+}
 
-// buttonElement.disabled = false
+async function getAllData(artist, song) {
+  const lyric_api = 'https://api.lyrics.ovh/v1/'
+  const image_api = 'https://imsea.herokuapp.com/api/1?q='
+
+  let lyrics = undefined
+  let picture = undefined
+
+  const lyric_url = lyric_api + artist + '/' + song
+  const image_url = image_api + artist
+
+  await fetchData(lyric_url).then((data) => {
+    if (data) {
+      lyrics = data.lyrics
+    }
+  })
+
+  await fetchData(image_url).then((data) => {
+    if (data) {
+      picture = data.results[0]
+    }
+  })
+
+  return { lyric: lyrics, picture: picture }
+
+  // console.log(lyrics, picture)
+}
+
+function renderData(artist, song, data) {
+  const artist_picture = document.querySelector('.search__image')
+  const artist_name_text = document.querySelector('.search__artist')
+  const song_name_text = document.querySelector('.search__song')
+  const lyrics_text = document.querySelector('.search__lyrics')
+
+  artist_picture.src = data.picture
+  artist_name_text.textContent = artist.toUpperCase()
+  song_name_text.textContent = song.toUpperCase()
+  lyrics_text.textContent = data.lyric
+}
+
+function start() {
+  const textFields = document.querySelectorAll('.mdc-text-field')
+  const button = document.querySelector('.mdc-button')
+
+  const buttonRipple = new MDCRipple(button)
+  const artist_text_field = new MDCTextField(textFields[0])
+  const song_text_field = new MDCTextField(textFields[1])
+
+  button.addEventListener('click', () => {
+    const artist = artist_text_field.value
+    const song = song_text_field.value
+
+    if (!artist || !song) return
+
+    getAllData(artist, song).then((data) => {
+      console.log(data)
+      renderData(artist, song, data)
+    })
+  })
+}
+
+start()
